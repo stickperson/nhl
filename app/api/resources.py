f8674086dfef4132ed30d9ed8fdb4c91153452b6
stickperson <joe.meissler@gmail.com>
2014-02-10 14:05:29 -0800

    hacked together league averages endpoint

--- app/api/resources.py
+++ app/api/resources.py
@@ -1,4 +1,5 @@
 from tastypie.resources import ModelResource, ALL
+from django.db.models import Avg
 from tastypie import fields
 from app.models import Team, Player
 from tastypie.bundle import Bundle
@@ -22,7 +23,7 @@ class TeamSimpleResource(ModelResource):
     class Meta:
         queryset = Team.objects.all()
         resource_name = 'teams'
-        fields = ['location', 'name', 'cap_total']
+        fields = ['location', 'name', 'cap_total', 'center_cap', 'lw_cap', 'rw_cap', 'd_cap', 'g_cap']
         limit = 35
 
 class DefenseSimpleResource(ModelResource):
@@ -58,19 +59,29 @@ class TeamResource(ModelResource):
         bundle.data['goalies'] = [pr._serialize(bundle.request, o) for o in goalies]
         return bundle
 
-    # def get_object_list(self, request):
-    #     self.fields['defense'].attribute = lambda bundle: Player.objects.filter(position='D', team=bundle.obj.id)
-    #     team = super(TeamResource, self).get_object_list(request).filter(players__team__name='flyers')
-    #     print self.id
-    #     print type(self.id)
-    #     return team
+class Stats(ModelResource):
 
-    # def dehydrate(self, bundle):
-    #     team = bundle.data['id']
-    #     bundle.data['centers'] = Player.objects.filter(team=team).filter(position='C')
-    #     bundle.data['goalies'] = Player.objects.filter(team=team).filter(position='G')
-    #     bundle.data['lws'] = Player.objects.filter(team=team).filter(position='LW')
-    #     bundle.data['rws'] = Player.objects.filter(team=team).filter(position='RW')
-    #     bundle.data['d'] = Player.objects.filter(team=team).filter(position='D')
-    #     return bundle
+    class Meta:
+        queryset = Team.objects.all()
+        resource_name = 'stats'
+        limit = 1
+        fields = ['id']
 
+    def dehydrate(self, bundle):
+        bundle = super(Stats, self).dehydrate(bundle)
+        c = Team.objects.all().aggregate(Avg('center_cap'))
+        c = round((c['center_cap__avg']/1000000), 3)
+        bundle.data['c_avg'] = c
+        lw = Team.objects.all().aggregate(Avg('lw_cap'))
+        lw = round((lw['lw_cap__avg']/1000000), 3)
+        bundle.data['lw_avg'] = lw
+        rw = Team.objects.all().aggregate(Avg('rw_cap'))
+        rw = round((rw['rw_cap__avg']/1000000), 3)
+        bundle.data['rw_avg'] = rw
+        g = Team.objects.all().aggregate(Avg('g_cap'))
+        g = round((g['g_cap__avg']/1000000), 3)
+        bundle.data['g_avg'] = g
+        d = Team.objects.all().aggregate(Avg('d_cap'))
+        d = round((d['d_cap__avg']/1000000), 3)
+        bundle.data['d_avg'] = d
+        return bundle
--- salaries/urls.py
+++ salaries/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from app.api.resources import PlayerSimpleResource, TeamSimpleResource, TeamResource, DefenseSimpleResource
+from app.api.resources import PlayerSimpleResource, TeamSimpleResource, TeamResource, DefenseSimpleResource, Stats
 
 v1_api = Api(api_name='v1')
 v1_api.register(TeamSimpleResource())
 v1_api.register(PlayerSimpleResource())
 v1_api.register(TeamResource())
 v1_api.register(DefenseSimpleResource())
+v1_api.register(Stats())
 
 from django.contrib import admin
 admin.autodiscover()
